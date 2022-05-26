3665eba8484ff5bee51be69d41afaf12d93bd1a1
Pawel <synk@poczta.onet.pl>
2022-05-26 22:38:35 +0200

    Add code for Response configuration lesson
    Lesson showing how to configure response status.

---
+++ api-konfiguracja-odpowiedzi/additonal_info.md
@@ -0,0 +1,3 @@
+Dodatkowe źródła
+
+Dokumentacja klasy ResponseEntity: https://docs.spring.io/spring-framework/docs/current/javadoc-api/org/springframework/http/ResponseEntity.html
---
+++ api-negocjacja-reprezentacji-zasobu/src/test/java/pl/pdob/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pdob.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
