0c7f332b161ff0366b4c6059cb387e758af0c8e5
Pawel <synk@poczta.onet.pl>
2022-06-24 14:49:39 +0200

    Add code for Validation integration with Hibernate and Spring Data
    Lesson showing how to use Validation with Hibernate and Spring Data

---
+++ walidacja-integracja-jpa/src/main/java/pl/pdob/validation/ClientRepository.java
@@ -0,0 +1,7 @@
+package pl.pdob.validation;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClientRepository extends CrudRepository<Client, Long> {
+    boolean existsByUsername(String username);
+}
