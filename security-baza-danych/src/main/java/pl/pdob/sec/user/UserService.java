a22fd271de567633f33e29adfcdfe79a3f0b1335
Pawel <synk@poczta.onet.pl>
2022-06-27 22:01:21 +0200

    Add code for Spring Security. Login using database.
    Lesson showing how to login using database in Spring Security.

---
+++ security-baza-danych/src/main/java/pl/pdob/sec/user/UserService.java
@@ -0,0 +1,21 @@
+package pl.pdob.sec.user;
+
+import org.springframework.stereotype.Service;
+import pl.pdob.sec.user.dto.UserCredentialsDto;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Optional<UserCredentialsDto> findCredentialsByEmail(String email) {
+        return userRepository.findByEmail(email)
+                .map(UserCredentialsDtoMapper::map);
+    }
+}
